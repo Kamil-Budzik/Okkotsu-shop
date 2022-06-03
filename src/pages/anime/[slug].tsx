69d69b5012aef686c47bc4c832f18c8d18bdd451
DonquixoteSabo <mixboksu@gmail.com>
2022-06-03 15:54:02 +0200

    feat: Fetch anime Data from CMS

--- src/pages/anime/[slug].tsx
+++ src/pages/anime/[slug].tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import { gql, GraphQLClient } from 'graphql-request';
 
-const AnimePage = () => {
+import IExtendedAnime from 'interfaces/IExtendedAnime';
+
+interface Props {
+  isError: boolean;
+  anime: IExtendedAnime;
+}
+
+const AnimePage = ({ anime, isError }: Props) => {
+  console.log(anime);
   const router = useRouter();
   const { slug } = router.query;
 
@@ -10,6 +19,43 @@ const AnimePage = () => {
       <h1>Anime page {slug}</h1>
     </div>
   );
+};
+
+interface Params {
+  params: {
+    slug: string;
+  };
+}
+
+export const getServerSideProps = async ({ params: { slug } }: Params) => {
+  try {
+    const animeQuery = gql`
+      query MyQuery {
+        anime(where: { slug: "${slug}" }) { 
+          id
+          title
+          desc
+          images {
+            id
+            url
+          }
+        }
+      }
+    `;
+
+    const client = new GraphQLClient(
+      'https://api-eu-central-1.graphcms.com/v2/cl3vrwvusfviq01z1bph11p9v/master'
+    );
+    const data = await client.request(animeQuery);
+
+    return {
+      props: { anime: data.anime, isError: false },
+    };
+  } catch {
+    return {
+      props: { anime: [], isError: true },
+    };
+  }
 };
 
 export default AnimePage;
