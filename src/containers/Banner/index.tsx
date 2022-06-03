533753d7c5f244dc69541c7c2d72136903370603
DonquixoteSabo <mixboksu@gmail.com>
2022-06-03 15:47:18 +0200

    feat: Add links to single anime page

--- src/containers/Banner/index.tsx
+++ src/containers/Banner/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -17,19 +18,23 @@ const Banner = ({ link, img }: IBanner) => {
           objectFit: 'cover',
         }}
       />
-      <Button
-        variant="contained"
-        sx={{
-          position: 'absolute',
-          left: '10%',
-          bottom: '10%',
-          width: 250,
-          fontSize: '1.5rem',
-          borderRadius: 30,
-        }}
-      >
-        Watch now
-      </Button>
+      <Link href={`/anime/${link}`}>
+        <a>
+          <Button
+            variant="contained"
+            sx={{
+              position: 'absolute',
+              left: '10%',
+              bottom: '10%',
+              width: 250,
+              fontSize: '1.5rem',
+              borderRadius: 30,
+            }}
+          >
+            Watch now
+          </Button>
+        </a>
+      </Link>
     </Container>
   );
 };
