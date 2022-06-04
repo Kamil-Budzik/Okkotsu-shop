d71780ca09d3f9851ac2854ef091fd0466e09e54
DonquixoteSabo <mixboksu@gmail.com>
2022-06-04 06:43:31 +0200

    refactor: Split Anime Slider into smaller components

--- src/pages/anime/[slug].tsx
+++ src/pages/anime/[slug].tsx
@@ -1,18 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { gql, GraphQLClient } from 'graphql-request';
 
 import IExtendedAnime from 'interfaces/IExtendedAnime';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import StarIcon from '@mui/icons-material/Star';
-import NoSsr from 'helpers/noSsr';
-import generateStars from 'helpers/generateStars';
 
+import SingleAnimeImages from 'components/SingleAnimeImages';
+import SingleAnimeCard from 'components/SingleAnimeCard';
+import SingleAnimeDesc from 'components/SingleAnimeDesc';
+ 
 interface Props {
   isError: boolean;
   anime: IExtendedAnime;
@@ -20,7 +16,6 @@ interface Props {
 
 const AnimePage = ({ anime, isError }: Props) => {
   const { title, images, desc, intro, price, stars } = anime;
-  const [activeImgIndex, setActiveImgIndex] = useState(0);
 
   return (
     <Container
@@ -33,95 +28,15 @@ const AnimePage = ({ anime, isError }: Props) => {
     >
       <div>
         <Grid container spacing={12}>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            sx={{ display: 'grid', justifyItems: 'center' }}
-          >
-            <Box
-              component="img"
-              sx={{ objectFit: 'cover', width: '100%', borderRadius: '15px' }}
-              src={images[activeImgIndex].url}
-              alt={title}
-            />
-            <List
-              sx={{
-                display: 'flex',
-                width: '100%',
-                justifyContent: 'space-evenly',
-              }}
-            >
-              {images.map((image, index) => (
-                <ListItem key={image.id}>
-                  <Box
-                    onClick={() => setActiveImgIndex(index)}
-                    component="img"
-                    src={image.url}
-                    alt={title}
-                    width={120}
-                    sx={{
-                      borderRadius: '15px',
-                      cursor: 'pointer',
-                      transition: '.4s ease-in-out',
-                      '&:hover': {
-                        transform: 'scale(1.1)',
-                      },
-                    }}
-                  />
-                </ListItem>
-              ))}
-            </List>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <header>
-              <Typography
-                variant="h3"
-                component="h1"
-                fontWeight={700}
-                gutterBottom
-              >
-                {title}
-              </Typography>
-              <Box sx={{ margin: '1em 0' }}>
-                {}
-                {generateStars(stars)}
-              </Box>
-              <Typography variant="body2" component="p">
-                {intro}
-              </Typography>
-            </header>
-            <Typography variant="h4" component="p" mt={2} mb={2}>
-              ${price}
-            </Typography>
-            <Box component="div" sx={{ display: 'flex', marginTop: '1em' }}>
-              <Button variant="outlined">Add to basket</Button>
-              <Button variant="contained" sx={{ marginLeft: '1em' }}>
-                Watch now
-              </Button>
-            </Box>
-          </Grid>
+          <SingleAnimeImages title={title} images={images} />
+          <SingleAnimeCard
+            title={title}
+            stars={stars}
+            intro={intro}
+            price={price}
+          />
         </Grid>
-        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-          <Box sx={{ maxWidth: '800px', marginTop: '5em' }}>
-            <Typography
-              variant="h4"
-              component="h2"
-              fontWeight={500}
-              gutterBottom
-            >
-              Description:
-            </Typography>
-            <NoSsr>
-              <Typography variant="body2" component="p">
-                <span
-                  dangerouslySetInnerHTML={{ __html: desc.html }}
-                  className="desc"
-                />
-              </Typography>
-            </NoSsr>
-          </Box>
-        </Box>
+        <SingleAnimeDesc desc={desc} />
       </div>
     </Container>
   );
